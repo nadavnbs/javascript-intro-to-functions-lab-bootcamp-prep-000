fcf8d238f1734e12b64911f2599adab5c0034e31
nadav ben shimon <nadavnbs@gmail.com>
2018-03-14 18:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,10 @@ function sayHiToGandma(string){
   if (string.toLowerCase() === string){
     return "I can't hear you!"
   }
+  else if (string.toUpperCase() === string){
+    return "YAS INDEED!"
+  }
+  else if (string === "I love you, Grandma"){
+    return "I love you, too"
+  }
 }
