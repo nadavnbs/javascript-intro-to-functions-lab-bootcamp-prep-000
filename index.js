38d7425fff50fb308060c849e42474604467a224
nadav ben shimon <nadavnbs@gmail.com>
2018-03-14 18:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGandma(string){
-  if (string === string.toLowerCase()){
+  if (string.toLowerCase() === string){
     return "I can't hear you!"
   }
 }
